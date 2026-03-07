ef509d0781790c7386d477c3c47d9cda9d35f8de
marbl <chris.smith.sm5532@gmail.com>
2026-03-07 19:41:39 +0000

    ForeCast complete

--- WeatherForecast/backend.py
+++ WeatherForecast/backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = 'f91d1d304950deffbb9e0fdf327c20da'
 
-def get_data(place,forecast_days=None,type=None):
+def get_data(place,forecast_days=None):
     url = (f'http://api.openweathermap.org/data/2.5/forecast?q={place}'
            f'&appid={API_KEY}')
     response = requests.get(url)
@@ -10,16 +10,8 @@ def get_data(place,forecast_days=None,type=None):
     sections = 8 * forecast_days
     filtered_data = data["list"]
     filtered_data = filtered_data[:sections]
-    if type == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-        return filtered_data
-    if type == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-        return filtered_data
-
-
-    return data
+    return filtered_data
 
 if __name__ == "__main__":
-    testdata = get_data(place="London",forecast_days=3,type="Temperature")
+    testdata = get_data(place="London",forecast_days=3)
     print(testdata)
--- WeatherForecast/main.py
+++ WeatherForecast/main.py
@@ -2,9 +2,10 @@ import streamlit as st
 import plotly.express as px
 from pandas.core.resample import get_resampler
 
-from backend import getData
+from backend import get_data
 from plotly.graph_objs.surface.contours import y
 
+# add UI
 st.title("Weather Forecast for Next days")
 place = st.text_input("Place: ")
 days = st.slider("ForeCast", 1, 5,
@@ -13,9 +14,26 @@ option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d,t = getData(place,days,option)
+#get Temperature/Sky data
+if place:
+    try:
+        filtered_data = get_data(place,days)
+        if option == "Temperature":
+            #plot Temperature Data
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            corrected_temp = [t / 10 for t in temperatures]
+            figure = px.line(x = dates, y= corrected_temp, labels= {"x":"Date","y":"Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            #Display Sky data
+            image_list = []
+            for condition in sky_conditions:
+                image_list.append(f"images/{condition}.png")
+            st.image(image_list,width=115)
+    except KeyError:
+            st.write("That place does not exist")
 
 
 
-figure = px.line(x = d,y=t,labels= {"x":"Date","y":"Temperature (C)"})
-st.plotly_chart(figure)
