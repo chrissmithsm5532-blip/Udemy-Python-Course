a4ccf006ac69fc8f0b6a63fe325b28fa6b4e9c34
marbl <chris.smith.sm5532@gmail.com>
2026-03-07 18:05:02 +0000

    ForeCast backend

--- WeatherForecast/main.py
+++ WeatherForecast/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from pandas.core.resample import get_resampler
+
+from backend import getData
 from plotly.graph_objs.surface.contours import y
 
 st.title("Weather Forecast for Next days")
@@ -10,13 +13,9 @@ option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates= ["2022-25-10","2022-26-10","2022-27-10"]
-    temperatures = [10,11,15]
-    temperatures = [days * i for i in temperatures]
-    return dates,temperatures
+d,t = getData(place,days,option)
 
-d,t = get_data(days)
+
 
 figure = px.line(x = d,y=t,labels= {"x":"Date","y":"Temperature (C)"})
 st.plotly_chart(figure)
