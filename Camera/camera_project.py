8dd62a5ea3ff66243b4797e86228139978dd0ff0
marbl <chris.smith.sm5532@gmail.com>
2026-03-12 23:24:20 +0000

    Camera App

---
+++ Camera/camera_project.py
@@ -0,0 +1,32 @@
+from tkinter import Image
+
+import cv2
+import streamlit as st
+from datetime import datetime
+
+
+
+
+st.title("Motion Detector")
+start = st.button("Start Camera")
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        check, frame = camera.read()
+        framc = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        day = datetime.now().strftime("%A")
+        time = datetime.now().strftime("%H:%M:%S")
+
+        cv2.ptText(img=frame,text=day,org=(30,80),
+                   fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=3,
+                   color=(255,255,255),
+                   thickness=2,lineType=cv2.LINE_AA)
+        cv2.ptText(img=frame, text=time, org=(30, 140),
+                   fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=3,
+                   color=(255, 0, 0),
+                   thickness=2, lineType=cv2.LINE_AA)
+
+        streamlit_image.image(frame)
