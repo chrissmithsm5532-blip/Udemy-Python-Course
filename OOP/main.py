0b4b06c19968226d32fd587e97812a517fb965eb
chrissmithsm5532-blip <chris.smith.sm5532@gmail.com>
2026-03-20 16:54:32 +0000

    Remove hardcoded email credentials from send method
    Removed hardcoded email credentials and receiver.

--- OOP/main.py
+++ OOP/main.py
@@ -32,10 +32,7 @@ class Email:
         host = "smtp.gmail.com"
         port = 465
 
-        username = "app8flask@gmail.com"
-        password = "qyciukmocfaiarse"
 
-        receiver = "app8flask@gmail.com"
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(host, port, context=context) as server:
             server.login(username, password)
