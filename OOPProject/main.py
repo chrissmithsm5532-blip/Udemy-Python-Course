c1a26d8e0c90b2e4b4f69410a4020d63698b81fc
marbl <chris.smith.sm5532@gmail.com>
2026-03-26 18:18:07 +0000

    OOPProject

---
+++ OOPProject/main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import fpdf as FPDF
+
+df = pd.read_csv('articles.csv',dtype={"id": str})
+
+class Article:
+    def __init__(self,article_id):
+        self.id = article_id
+        self.name = df.loc[df['id'] == self.id,'name'].squeeze()
+        self.price = df.loc[df['id'] == self.id,'price'].squeeze()
+
+    def available(self):
+        in_stock =  df.loc[df['id'] == self.id,'in_stock'].squeeze()
+        print(in_stock)
+        return in_stock
+
+class Receipt:
+    def __init__(self,article):
+        self.article = article
+
+    def generate(self):
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.1", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: Laptop Sven", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: 999", ln=1)
+
+        pdf.output("receipt.pdf")
+
+
+print(df)
+userinput = input ("Enter ID to purchase: ")
+article = Article(article_id=userinput)
+if article.available():
+    receipt = Receipt(article=article)
+    receipt.generate()
+else:
+    print("article not available")
+
